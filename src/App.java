1e3ce801343ef9ef95354bffda34ed0f2171c7be
bmontellano <bmontellano@gmail.com>
2021-12-22 09:53:15 -0800

    Added static method for Calculator

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import methods.Calculator;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Hello, User!");
+
+        //We can call this static method without instantiating the Calculator class
+        Calculator.printHelloWorld();
     }
 }
--- src/methods/Calculator.java
+++ src/methods/Calculator.java
@@ -5,4 +5,30 @@ public class Calculator {
     public double cubedRoot(double x) {
         return Math.pow((1/3), x);
     }
+
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return
+     */
+    public double addThese(double x, double y){
+        return x + y;
+    }
+
+    /**
+     * OVERLOAD
+     * @param x
+     * @param y
+     * @param z
+     * @return
+     */
+    public double addThese(double x, double y, double z){
+        return x + y + z;
+    }
+
+    public static void printHelloWorld() {
+        System.out.println("Hello World!!");
+    }
+
 }
